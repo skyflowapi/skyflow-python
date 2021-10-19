742dedbfb31f557456911c7a7e01904894f3402b
Tejesh <tejesh.allampati@skyflow.com>
2021-10-19 10:21:49 +0530

    added tests

--- skyflow/ServiceAccount/_token.py
+++ skyflow/ServiceAccount/_token.py
@@ -3,19 +3,24 @@ import json
 import jwt
 import datetime
 import requests
+from requests import status_codes
+
+from skyflow.Errors._skyflowErrors import *
 
 ResponseToken = namedtuple('ResponseToken', ['AccessToken', 'TokenType'])
 
 def GenerateToken(credentialsFilePath):
     try:
         credentialsFile = open(credentialsFilePath, 'r')
-    except e:
-        raise Exception("Credentials file not found")
+    except:
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.FILE_NOT_FOUND.value % (credentialsFilePath))
 
     try:
         credentials = json.load(credentialsFile)
-    except e:
-        raise Exception("Credentials file is not in JSON format")
+    except Exception as e:
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.FILE_INVALID_JSON.value % (credentialsFilePath))
+    finally:
+        credentialsFile.close()
 
 
     return getSAToken(credentials)
@@ -24,26 +29,30 @@ def getSAToken(credentials):
     try:
         privateKey = credentials["privateKey"]
     except:
-        raise Exception("Private key not found in credentials")
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.MISSING_PRIVATE_KEY)
     try:
         clientID = credentials["clientID"]
     except:
-        raise Exception("Client ID not found in credentials")
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.MISSING_CLIENT_ID)
     try:
         keyID = credentials["keyID"]
     except:
-        raise Exception("Key ID not found in credentials")
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.MISSING_KEY_ID)
     try:
         tokenURI = credentials["tokenURI"]
     except:
-        raise Exception("Token URI not found in credentials")
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.MISSING_TOKEN_URI)
 
     signedToken = getSignedJWT(clientID, keyID, tokenURI, privateKey)
 
     response = sendRequestWithToken(tokenURI, signedToken) 
 
-    token = json.loads(response.content)
-    return ResponseToken(AccessToken=token["accessToken"], TokenType=token["tokenType"])
+    try:
+        token = json.loads(response.content)
+    except json.decoder.JSONDecodeError as e:
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, "Unable to parse the response")
+    return getResponseToken(token)
+    
 
 def getSignedJWT(clientID, keyID, tokenURI, privateKey):
     payload = {
@@ -55,8 +64,8 @@ def getSignedJWT(clientID, keyID, tokenURI, privateKey):
     }
     try:
         return jwt.encode(payload=payload, key=privateKey, algorithm="RS256")
-    except:
-        raise Exception("Unable to get a JWT with the given credentials")
+    except Exception as e:
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.JWT_INVALID_FORMAT)
 
 
 def sendRequestWithToken(url, token):
@@ -67,7 +76,29 @@ def sendRequestWithToken(url, token):
         "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
 		"assertion":  token
     }
-    response = requests.post(url=url, json=payload, headers=headers)
+    try:
+        response = requests.post(url=url, json=payload, headers=headers)
+        statusCode = response.status_code
+    except requests.exceptions.InvalidURL:
+        raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.INVALID_URL.value % (url))
+
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        raise SkyflowError(statusCode, error.strerror)
 
     return response
 
+def getResponseToken(token):
+    try:
+        accessToken = token["accessToken"]
+    except:
+        raise SkyflowError(SkyflowErrorCodes.SERVER_ERROR, SkyflowErrorMessages.MISSING_ACCESS_TOKEN)
+    
+    try:
+        tokenType = token["tokenType"]
+    except:
+        raise SkyflowError(SkyflowErrorCodes.SERVER_ERROR, SkyflowErrorMessages.MISSING_TOKEN_TYPE)
+
+    return ResponseToken(AccessToken=accessToken, TokenType=tokenType)
---
+++ tests/ServiceAccount/testGenerateToken.py
@@ -0,0 +1,63 @@
+import unittest
+import os
+from skyflow.ServiceAccount import GenerateToken
+from skyflow.Errors._skyflowErrors import *
+
+class TestGenerateToken(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.dataPath = os.path.join(os.getcwd(), 'tests/ServiceAccount/data/')
+        return super().setUp()
+    
+    def getDataPath(self, file):
+        return self.dataPath + file + '.json'
+
+    def testWithInvalidFilePath(self):
+        try:
+            GenerateToken('unknownfilepath')
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.FILE_NOT_FOUND.value % ('unknownfilepath'))
+    
+
+    def testInvalidJSON(self):
+        path = self.getDataPath('empty')
+        try:
+            GenerateToken(path)
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.FILE_INVALID_JSON.value % (path))
+
+    def testWithNoPrivateKey(self):
+        try:
+            GenerateToken(self.getDataPath('noPrivateKey'))
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.MISSING_PRIVATE_KEY.value)
+    
+    def testWithNoClientID(self):
+        try:
+            GenerateToken(self.getDataPath('noClientID'))
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.MISSING_CLIENT_ID.value)
+
+    def testWithNoKeyID(self):
+        try:
+            GenerateToken(self.getDataPath('noKeyID'))
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.MISSING_KEY_ID.value)
+
+    def testWithNoTokenURI(self):
+        try:
+            GenerateToken(self.getDataPath('noTokenURI'))
+        except SkyflowError as se:
+            self.assertEqual(se.code, SkyflowErrorCodes.INVALID_INPUT.value)
+            self.assertEqual(se.message, SkyflowErrorMessages.MISSING_TOKEN_URI.value)
+
+    def testInvalidCreds(self):
+        try:
+            result = GenerateToken(self.getDataPath('invalidPrivateKey'))
+        except SkyflowError as e:
+            print(e)
