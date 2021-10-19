54a083764db0ffc0a0cc5206e22bc7226726f7f2
Tejesh <tejesh.allampati@skyflow.com>
2021-10-19 14:16:34 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-version = '1.0.0'
+current_version = '1.0.0'
 
 setup(
    name='skyflow',
-   version=version,
+   version=current_version,
    author='Skyflow',
    author_email='service-ops@skyflow.com',
-   packages=find_packages(exclude=['tests', 'examples']),
+   packages=find_packages(where='.', exclude=['test*']),
    url='https://github.com/skyflowapi/skyflow-python/',
    license='LICENSE',
    description='Skyflow SDK for the Python programming language',
