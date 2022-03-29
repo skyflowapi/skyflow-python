e4a0d29ef7a6887718e3f6fbda441b8ef7a23645
skyflow-tejesh-reddy <86997821+skyflow-tejesh-reddy@users.noreply.github.com>
2022-03-29 16:25:17 +0530

    Release/29 03 2022 (#47)
    * fix header override differing case issue
    
    * refactor vault url
    
    * fix detokenize url
    
    * add validation to tokenProvider
    
    * fix token verification, tests
    
    * Add links to samples
    
    * Remove old doc site link
    
    * Update CHANGELOG.md
    
    * Update setup.py
    
    Co-authored-by: yennamsanthosh-skyflow <84496972+yennamsanthosh-skyflow@users.noreply.github.com>

--- skyflow/vault/_client.py
+++ skyflow/vault/_client.py
@@ -10,21 +10,25 @@ import asyncio
 from skyflow.errors._skyflowerrors import SkyflowError, SkyflowErrorCodes, SkyflowErrorMessages
 from skyflow._utils import log_info, InfoMessages, InterfaceName
 from ._token import tokenProviderWrapper
+
+
 class Client:
     def __init__(self, config: Configuration):
-        
+
         interface = InterfaceName.CLIENT.value
 
         log_info(InfoMessages.INITIALIZE_CLIENT.value, interface=interface)
 
-        
         if not isinstance(config.vaultID, str):
-            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.VAULT_ID_INVALID_TYPE.value%(str(type(config.vaultID))), interface=interface)
+            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.VAULT_ID_INVALID_TYPE.value % (
+                str(type(config.vaultID))), interface=interface)
         if not isinstance(config.vaultURL, str):
-            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.VAULT_URL_INVALID_TYPE.value%(str(type(config.vaultURL))), interface=interface)
+            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.VAULT_URL_INVALID_TYPE.value % (
+                str(type(config.vaultURL))), interface=interface)
 
         if not isinstance(config.tokenProvider, types.FunctionType):
-            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.TOKEN_PROVIDER_ERROR.value%(str(type(config.tokenProvider))), interface=interface)
+            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.TOKEN_PROVIDER_ERROR.value % (
+                str(type(config.tokenProvider))), interface=interface)
 
         self.vaultID = config.vaultID
         self.vaultURL = config.vaultURL.rstrip('/')
@@ -39,8 +43,9 @@ class Client:
         self._checkConfig(interface)
 
         jsonBody = getInsertRequestBody(records, options.tokens)
-        requestURL = self.vaultURL + "/v1/vaults/" + self.vaultID
-        self.storedToken = tokenProviderWrapper(self.storedToken, self.tokenProvider, interface)
+        requestURL = self._get_complete_vault_url()
+        self.storedToken = tokenProviderWrapper(
+            self.storedToken, self.tokenProvider, interface)
         headers = {
             "Authorization": "Bearer " + self.storedToken
         }
@@ -52,60 +57,80 @@ class Client:
         log_info(InfoMessages.INSERT_DATA_SUCCESS.value, interface)
         return result
 
+    def detokenize(self, records):
+        interface = InterfaceName.DETOKENIZE.value
+        log_info(InfoMessages.DETOKENIZE_TRIGGERED.value, interface)
+
+        self._checkConfig(interface)
+        self.storedToken = tokenProviderWrapper(
+            self.storedToken, self.tokenProvider, interface)
+        url = self._get_complete_vault_url() + '/detokenize'
+        responses = asyncio.run(sendDetokenizeRequests(
+            records, url, self.storedToken))
+        result, partial = createDetokenizeResponseBody(responses)
+        if partial:
+            raise SkyflowError(SkyflowErrorCodes.PARTIAL_SUCCESS,
+                               SkyflowErrorMessages.PARTIAL_SUCCESS, result, interface=interface)
+        else:
+            log_info(InfoMessages.DETOKENIZE_SUCCESS.value, interface)
+            return result
+
+    def get_by_id(self, records):
+        interface = InterfaceName.GET_BY_ID.value
+        log_info(InfoMessages.GET_BY_ID_TRIGGERED.value, interface)
+
+        self._checkConfig(interface)
+        self.storedToken = tokenProviderWrapper(
+            self.storedToken, self.tokenProvider, interface)
+        url = self._get_complete_vault_url()
+        responses = asyncio.run(sendGetByIdRequests(
+            records, url, self.storedToken))
+        result, partial = createGetByIdResponseBody(responses)
+        if partial:
+            raise SkyflowError(SkyflowErrorCodes.PARTIAL_SUCCESS,
+                               SkyflowErrorMessages.PARTIAL_SUCCESS, result, interface=interface)
+        else:
+            log_info(InfoMessages.GET_BY_ID_SUCCESS.value, interface)
+
+            return result
+
     def invoke_connection(self, config: ConnectionConfig):
-        
+
         interface = InterfaceName.INVOKE_CONNECTION.value
         log_info(InfoMessages.INVOKE_CONNECTION_TRIGGERED.value, interface)
 
         self._checkConfig(interface)
         session = requests.Session()
-        self.storedToken = tokenProviderWrapper(self.storedToken, self.tokenProvider, interface)
+        self.storedToken = tokenProviderWrapper(
+            self.storedToken, self.tokenProvider, interface)
         request = createRequest(config)
 
-        if not 'X-Skyflow-Authorization' in request.headers.keys():
+        lowercase_headers = [header.lower()
+                             for header in request.headers.keys()]
+
+        if not 'X-Skyflow-Authorization'.lower() in lowercase_headers:
             request.headers['X-Skyflow-Authorization'] = self.storedToken
 
-        if not 'Content-Type' in request.headers.keys():
+        if not 'Content-Type'.lower() in lowercase_headers:
             request.headers['Content-Type'] = 'application/json'
 
         response = session.send(request)
         session.close()
         return processResponse(response, interface=interface)
 
-    def detokenize(self, records):
-        interface = InterfaceName.DETOKENIZE.value
-        log_info(InfoMessages.DETOKENIZE_TRIGGERED.value, interface)
-
-        self._checkConfig(interface)
-        self.storedToken = tokenProviderWrapper(self.storedToken, self.tokenProvider, interface)
-        url = self.vaultURL + "/v1/vaults/" + self.vaultID + "/detokenize"
-        responses = asyncio.run(sendDetokenizeRequests(records, url, self.storedToken))
-        result, partial = createDetokenizeResponseBody(responses)
-        if partial:
-            raise SkyflowError(SkyflowErrorCodes.PARTIAL_SUCCESS ,SkyflowErrorMessages.PARTIAL_SUCCESS, result, interface=interface)
-        else:
-            log_info(InfoMessages.DETOKENIZE_SUCCESS.value, interface)
-            return result
-    
-    def get_by_id(self, records):
-        interface = InterfaceName.GET_BY_ID.value
-        log_info(InfoMessages.GET_BY_ID_TRIGGERED.value, interface)
-
-        self._checkConfig(interface)
-        self.storedToken = tokenProviderWrapper(self.storedToken, self.tokenProvider, interface)
-        url = self.vaultURL + "/v1/vaults/" + self.vaultID
-        responses = asyncio.run(sendGetByIdRequests(records, url, self.storedToken))
-        result, partial = createGetByIdResponseBody(responses)
-        if partial:
-            raise SkyflowError(SkyflowErrorCodes.PARTIAL_SUCCESS ,SkyflowErrorMessages.PARTIAL_SUCCESS, result, interface=interface)
-        else:
-            log_info(InfoMessages.GET_BY_ID_SUCCESS.value, interface)
-            return result
-        
     def _checkConfig(self, interface):
+        '''
+            Performs basic check on the given client config
+        '''
         if not len(self.vaultID) > 0:
-            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.EMPTY_VAULT_ID, interface=interface)
+            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT,
+                               SkyflowErrorMessages.EMPTY_VAULT_ID, interface=interface)
         if not len(self.vaultURL) > 0:
-            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT, SkyflowErrorMessages.EMPTY_VAULT_URL, interface=interface)
+            raise SkyflowError(SkyflowErrorCodes.INVALID_INPUT,
+                               SkyflowErrorMessages.EMPTY_VAULT_URL, interface=interface)
 
-
+    def _get_complete_vault_url(self):
+        '''
+            Get the complete vault url from given vault url and vault id
+        '''
+        return self.vaultURL + "/v1/vaults/" + self.vaultID
