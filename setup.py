c0ad193d959359cd02ba93b5e1fbf1f0c04ab4db
skyflow-tejesh-reddy <skyflow-tejesh-reddy@users.noreply.github.com>
2022-06-28 09:27:18 +0000

    [AUTOMATED] Public Release - 1.6.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 if sys.version_info < (3, 7):
     raise RuntimeError("skyflow requires Python 3.7+")
-current_version = "1.6.1",
+current_version = "1.6.2",
 
 setup(
     name='skyflow',
