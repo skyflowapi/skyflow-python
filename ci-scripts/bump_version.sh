f284ef75bcc5e7456b65eaeec2c7128ce1c7e485
skyflow-tejesh-reddy <86997821+skyflow-tejesh-reddy@users.noreply.github.com>
2022-06-28 17:12:59 +0530

    [SDK-579] Fix bump_version script

--- ci-scripts/bump_version.sh
+++ ci-scripts/bump_version.sh
@@ -5,14 +5,14 @@ if [ -z $2 ]
 then
 	echo "Bumping package version to $1"
 
-	sed -E "s/current_version = .+/current_version = \"$SEMVER\",/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
+	sed -E "s/current_version = .+/current_version = \'$SEMVER\',/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
 	echo --------------------------
 	echo "Done, Package now at $1"
 
 else
 	echo "Bumping package version to $1-dev.$2"
 
-	sed -E "s/current_version = .+/current_version = \"$SEMVER-dev.$2\",/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
+	sed -E "s/current_version = .+/current_version = \'$SEMVER-dev.$2\',/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
 
 	echo --------------------------
 	echo "Done, Package now at $1-dev.$2"
