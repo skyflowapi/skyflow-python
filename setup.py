997854e1f1a9e4483d81282a96b09aba627a6740
skyflow-bharti <skyflow-bharti@users.noreply.github.com>
2023-08-25 10:31:42 +0000

    [AUTOMATED] Public Release - 1.11.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 if sys.version_info < (3, 7):
     raise RuntimeError("skyflow requires Python 3.7+")
-current_version = '1.10.1'
+current_version = '1.11.0'
 
 setup(
     name='skyflow',
--- skyflow/version.py
+++ skyflow/version.py
@@ -1,1 +1,1 @@
-SDK_VERSION = '1.10.1'
+SDK_VERSION = '1.11.0'
