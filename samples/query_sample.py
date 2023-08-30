f857c3edfe7bb9fede15f0f18df4bb64d6575487
skyflow-vivek <vivek.jain@skyflow.com>
2023-08-30 11:09:39 +0530

    SK-907 Query Support in Python SDK
    - SK-975 Updated README and CHANGELOG
    - SK-975 Added Sample for Query method

---
+++ samples/query_sample.py
@@ -0,0 +1,35 @@
+'''
+	Copyright (c) 2022 Skyflow, Inc.
+'''
+
+from skyflow import set_log_level, LogLevel
+from skyflow.errors import SkyflowError
+from skyflow.service_account import generate_bearer_token, is_expired
+from skyflow.vault import Client, Configuration
+
+# cache token for reuse
+bearerToken = ''
+
+def token_provider():
+    global bearerToken
+    if is_expired(bearerToken):
+        bearerToken, _ = generate_bearer_token('<YOUR_CREDENTIALS_FILE_PATH>')
+    return bearerToken
+
+try:
+    config = Configuration(
+       '<UOIR_VAULT_ID>', '<YOUR_VAULT_URL>', token_provider)
+    client = Client(config)
+    
+    set_log_level(LogLevel.DEBUG)
+    
+    data = {
+        "query": "<YOUR_SQL_QUERY>"
+    }
+    response = client.query(data)
+    print('Response:', response)
+except SkyflowError as e:
+    if(e.data):
+        print('Error Occurred:', e.data)
+    else:
+        print('Error Occurred:', e.message)
