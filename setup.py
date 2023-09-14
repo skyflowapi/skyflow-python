663b17e25db54dee0e435dead5f2370b9fad16ad
skyflow-vivek <skyflow-vivek@users.noreply.github.com>
2023-09-14 12:18:08 +0000

    [AUTOMATED] Public Release - 1.13.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 if sys.version_info < (3, 7):
     raise RuntimeError("skyflow requires Python 3.7+")
-current_version = '1.13.0'
+current_version = '1.13.1'
 
 setup(
     name='skyflow',
