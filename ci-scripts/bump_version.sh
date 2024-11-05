0dd7789e571e85b344d1189dd8d93f65c68ff493
saileshwar-skyflow <saileshwar.bogam@skyflow.com>
2024-11-05 20:24:16 +0530

    updated bump_version.sh

--- ci-scripts/bump_version.sh
+++ ci-scripts/bump_version.sh
@@ -12,11 +12,11 @@ then
 	echo "Done, Package now at $1"
 
 else
-	echo "Bumping package version to $1-dev.$2"
+	echo "Bumping package version to $1-dev+$2"
 
-	sed -E "s/current_version = .+/current_version = \'$SEMVER-dev.$2\'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
-	sed -E "s/SDK_VERSION = .+/SDK_VERSION = \'$SEMVER-dev.$2\'/g" skyflow/version.py > tempfile && cat tempfile > skyflow/version.py && rm -f tempfile
+	sed -E "s/current_version = .+/current_version = \'$SEMVER-dev+$2\'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
+	sed -E "s/SDK_VERSION = .+/SDK_VERSION = \'$SEMVER-dev+$2\'/g" skyflow/version.py > tempfile && cat tempfile > skyflow/version.py && rm -f tempfile
 
 	echo --------------------------
-	echo "Done, Package now at $1-dev.$2"
+	echo "Done, Package now at $1-dev+$2"
 fi
