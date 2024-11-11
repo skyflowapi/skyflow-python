087306425ed2824088b47d043cf02d212d316419
saileshwar-skyflow <saileshwar.bogam@skyflow.com>
2024-11-11 18:54:32 +0530

    SK-1731: Updated bump version yml file

--- ci-scripts/bump_version.sh
+++ ci-scripts/bump_version.sh
@@ -1,22 +1,25 @@
 Version=$1
 SEMVER=$Version
 
-if [ -z $2 ]
+if [ -z "$2" ]
 then
-	echo "Bumping package version to $1"
+    echo "Bumping package version to $1"
 
-	sed -E "s/current_version = .+/current_version = \'$SEMVER\'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
-	sed -E "s/SDK_VERSION = .+/SDK_VERSION = \'$SEMVER\'/g" skyflow/utils/_version.py > tempfile && cat tempfile > skyflow/utils/_version.py && rm -f tempfile
+    sed -E "s/current_version = .+/current_version = '$SEMVER'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
+    sed -E "s/SDK_VERSION = .+/SDK_VERSION = '$SEMVER'/g" skyflow/utils/_version.py > tempfile && cat tempfile > skyflow/utils/_version.py && rm -f tempfile
 
-	echo --------------------------
-	echo "Done, Package now at $1"
+    echo --------------------------
+    echo "Done, Package now at $1"
 
 else
-	echo "Bumping package version to $1-dev$2"
+    # Use dev version with commit SHA
+    DEV_VERSION="${SEMVER}.dev0+$(echo $2 | tr -dc '0-9a-f')"
 
-	sed -E "s/current_version = .+/current_version = \'$SEMVER-dev$2\'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
-	sed -E "s/SDK_VERSION = .+/SDK_VERSION = \'$SEMVER-dev$2\'/g" skyflow/utils/_version.py > tempfile && cat tempfile > skyflow/utils/_version.py && rm -f tempfile
+    echo "Bumping package version to $DEV_VERSION"
 
-	echo --------------------------
-	echo "Done, Package now at $1-dev$2"
+    sed -E "s/current_version = .+/current_version = '$DEV_VERSION'/g" setup.py > tempfile && cat tempfile > setup.py && rm -f tempfile
+    sed -E "s/SDK_VERSION = .+/SDK_VERSION = '$DEV_VERSION'/g" skyflow/utils/_version.py > tempfile && cat tempfile > skyflow/utils/_version.py && rm -f tempfile
+
+    echo --------------------------
+    echo "Done, Package now at $DEV_VERSION"
 fi
