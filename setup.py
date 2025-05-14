f38a37c14656391308202f7c5a619cd2fda00a82
saileshwar-skyflow <saileshwar-skyflow@users.noreply.github.com>
2025-05-14 14:45:17 +0000

    [AUTOMATED] Private Release 2.0.0b2.dev0+1816a92

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 
 if sys.version_info < (3, 8):
     raise RuntimeError("skyflow requires Python 3.8+")
-current_version = '2.0.0b2.dev0+3d817ab'
+current_version = '2.0.0b2.dev0+1816a92'
 
 setup(
     name='skyflow',
