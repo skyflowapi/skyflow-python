3e794e3979199f15638a3f3ced709b35e6bc4a69
saileshwar-skyflow <saileshwar.bogam@skyflow.com>
2025-11-07 12:50:23 +0530

    SK-2385: update output transcriptions enum

--- skyflow/utils/enums/detect_output_transcriptions.py
+++ skyflow/utils/enums/detect_output_transcriptions.py
@@ -4,5 +4,4 @@ class DetectOutputTranscriptions(Enum):
     DIARIZED_TRANSCRIPTION = "diarized_transcription"
     MEDICAL_DIARIZED_TRANSCRIPTION = "medical_diarized_transcription"
     MEDICAL_TRANSCRIPTION = "medical_transcription"
-    PLAINTEXT_TRANSCRIPTION = "plaintext_transcription"
     TRANSCRIPTION = "transcription"
