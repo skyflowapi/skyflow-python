a22b9c67b24d23c3b867c60d3b6547870a239b9c
skyflow-vivek <vivek.jain@skyflow.com>
2025-11-11 17:00:39 +0530

    SK-1773 Add str for file upload response

--- skyflow/vault/data/_file_upload_response.py
+++ skyflow/vault/data/_file_upload_response.py
@@ -4,3 +4,9 @@ class FileUploadResponse:
                  errors):
         self.skyflow_id = skyflow_id
         self.errors = errors
+    
+    def __repr__(self):
+        return f"FileUploadResponse(skyflow_id={self.skyflow_id}, errors={self.errors})"
+
+    def __str__(self):
+        return self.__repr__()
