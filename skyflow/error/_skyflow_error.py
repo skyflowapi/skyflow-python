e33ae928dc665af383cf8f806f19954326bd0375
skyflow-bharti <118584001+skyflow-bharti@users.noreply.github.com>
2026-02-19 19:01:58 +0530

    SK-2548 fix config validation (#235)
    * SK-2548 update config validation
    
    * SK-2548 fix the unit test cases

--- skyflow/error/_skyflow_error.py
+++ skyflow/error/_skyflow_error.py
@@ -15,5 +15,4 @@ class SkyflowError(Exception):
         self.http_status = http_status if http_status else SkyflowMessages.HttpStatus.BAD_REQUEST.value
         self.details = details
         self.request_id = request_id
-        log_error(message, http_code, request_id, grpc_code, http_status, details)
         super().__init__()
