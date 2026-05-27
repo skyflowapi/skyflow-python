8579188ee5dc5da845c043c3006ce3f02bd5f957
Devesh-Skyflow <Devesh-Skyflow@users.noreply.github.com>
2026-05-27 10:50:26 +0000

    [AUTOMATED] Private Release 2.1.1.dev0+3520034

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 if sys.version_info < (3, 7):
     raise RuntimeError("skyflow requires Python 3.7+")
-current_version = '1.15.6'
+current_version = '2.1.1.dev0+3520034'
 
 setup(
     name='skyflow',
